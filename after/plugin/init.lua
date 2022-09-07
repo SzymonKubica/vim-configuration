dc3feb6d8a2a4f9549da01b2b6b5ba2fda0a681f
Szymon Kubica <szymo.kubica@gmail.com>
2022-09-07 21:40:02 +0200

    Fixed comment formatting.

--- after/plugin/init.lua
+++ after/plugin/init.lua
@@ -1,4 +1,4 @@
--- Colorscheme Configuration -----------------------------------------------------------
+-- Colorscheme Configuration ---------------------------------------------------
 vim.g.tokyonight_transparent_sidebar = true
 vim.g.tokyonight_transparent = true
 vim.opt.background = "dark"
