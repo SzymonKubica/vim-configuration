ca8e2095b57272898ca82a0f972b211b02395bf4
Szymon Kubica <szymo.kubica@gmail.com>
2022-09-21 16:45:18 +0200

    Added the configuration for the haskell language server.

--- lua/szymon/remap.lua
+++ lua/szymon/remap.lua
@@ -48,7 +48,18 @@ local lsp_flags = {
   -- This is the default in Nvim 0.7+
   debounce_text_changes = 150,
 }
+
 require('lspconfig')['pyright'].setup{
     on_attach = on_attach,
     flags = lsp_flags,
+}
+
+require('lspconfig')['hls'].setup{
+	on_attach = on_attach,
+	settings = {
+		haskell = {
+				hlintOn = true,
+				formattingProvider = "fourmolu"
+		}
+	}
 }
