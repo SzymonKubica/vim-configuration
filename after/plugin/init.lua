7e26632c6b761050e1f0e4e87df3833a0350be10
Szymon Kubica <szymo.kubica@gmail.com>
2022-09-21 10:58:57 +0200

    Enabled the rust and haskell tresitter syntax highlighting.

--- after/plugin/init.lua
+++ after/plugin/init.lua
@@ -19,7 +19,7 @@ vim.g.UltiSnipsJumpBackwardTrigger = '<s-tab>'
 -- Treesitter Configuration ----------------------------------------------------
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "c", "lua", "rust", "python", "java" },
+  ensure_installed = { "c", "lua", "rust", "python", "java", "haskell" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
@@ -36,7 +36,7 @@ require'nvim-treesitter.configs'.setup {
 
 
 		-- List of parsers to disable the extension for
-    disable = { "c", "rust" },
+    disable = { "c" },
 
     additional_vim_regex_highlighting = false,
   },
@@ -49,4 +49,47 @@ require'nvim-treesitter.configs'.setup {
       node_decremental = "grm",
     },
   },
+}
+
+-- Renamer Configuration -------------------------------------------------------
+local mappings_utils = require('renamer.mappings.utils')
+require('renamer').setup {
+    -- The popup title, shown if `border` is true
+    title = 'Rename',
+    -- The padding around the popup content
+    padding = {
+        top = 0,
+        left = 0,
+        bottom = 0,
+        right = 0,
+    },
+    -- The minimum width of the popup
+    min_width = 15,
+    -- The maximum width of the popup
+    max_width = 45,
+    -- Whether or not to shown a border around the popup
+    border = true,
+    -- The characters which make up the border
+    border_chars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
+    -- Whether or not to highlight the current word references through LSP
+    show_refs = true,
+    -- Whether or not to add resulting changes to the quickfix list
+    with_qf_list = true,
+    -- Whether or not to enter the new name through the UI or Neovim's `input`
+    -- prompt
+    with_popup = true,
+    -- The keymaps available while in the `renamer` buffer. The example below
+    -- overrides the default values, but you can add others as well.
+    mappings = {
+        ['<c-i>'] = mappings_utils.set_cursor_to_start,
+        ['<c-a>'] = mappings_utils.set_cursor_to_end,
+        ['<c-e>'] = mappings_utils.set_cursor_to_word_end,
+        ['<c-b>'] = mappings_utils.set_cursor_to_word_start,
+        ['<c-c>'] = mappings_utils.clear_line,
+        ['<c-u>'] = mappings_utils.undo,
+        ['<c-r>'] = mappings_utils.redo,
+    },
+    -- Custom handler to be run after successfully renaming the word. Receives
+    -- the LSP 'textDocument/rename' raw response as its parameter.
+    handler = nil,
 }
