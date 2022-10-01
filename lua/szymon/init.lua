c3105dd93d01717c1a75c2198e5cbb4aa46d9291
SzymonKubica <szymo.kubica@gmail.com>
2022-10-01 23:22:39 +0200

    Added autostart commands for the nerdtree.

--- lua/szymon/init.lua
+++ lua/szymon/init.lua
@@ -7,3 +7,7 @@ require("szymon.lsp")
 
 -- Starts up NERDTree if no file arguments are given.
 vim.cmd("autocmd VimEnter * if !argc() | NERDTree | endif")
+
+-- Exit Vim if NERDTree is the only window remaining in the only tab.
+vim.cmd("autocmd BufEnter * if tabpagenr('$') == 1 " ..
+"&& winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() | quit | endif")
