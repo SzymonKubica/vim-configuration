a50255f13ada6582d8ed766f9de02d70a18b29f0
SzymonKubica <szymo.kubica@gmail.com>
2022-10-29 16:52:57 +0100

    Removed useless black formatting autocommand.

--- lua/szymon/autocmd.lua
+++ lua/szymon/autocmd.lua
@@ -6,8 +6,3 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   pattern = { "*" },
   command = [[%s/\s\+$//e]],
 })
--- Automatically formats python with black
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-  pattern = { "python" },
-  command = [[!black %]],
-})
