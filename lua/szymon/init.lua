3219b701809f01ab55138b328bc57280b1cd67f8
SzymonKubica <szymo.kubica@gmail.com>
2022-10-29 16:53:38 +0100

    Changed the setup to start NERDTree in full screen on startup.

--- lua/szymon/init.lua
+++ lua/szymon/init.lua
@@ -6,4 +6,4 @@ require("szymon.autocmd")
 require("szymon.lsp")
 
 -- Starts up NERDTree if no file arguments are given.
-vim.cmd("autocmd VimEnter * if !argc() | NERDTree | endif")
+vim.cmd("autocmd VimEnter * if !argc() | NERDTree | only | endif")
