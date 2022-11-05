91461d844091dbb0d4b44cafa96823332cbb715f
SzymonKubica <szymo.kubica@gmail.com>
2022-11-05 10:35:10 +0000

    Updated the keymappings for scrolling docs in autocomplete.

--- lua/szymon/lsp.lua
+++ lua/szymon/lsp.lua
@@ -4,7 +4,6 @@
 local nnoremap = require("szymon.keymap").nnoremap
 local inoremap = require("szymon.keymap").inoremap
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
 local lspconfig = require('lspconfig')
 
@@ -106,9 +105,9 @@ cmp.setup {
     end,
   },
   mapping = cmp.mapping.preset.insert({
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-f>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Tab>'] = cmp.mapping.complete(),
     ['<CR>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
