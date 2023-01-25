c481b31b7eb6de2db0898d1abf9d8b2f742fe766
SzymonKubica <szymo.kubica@gmail.com>
2023-01-25 22:09:01 +0000

    Reverted the colorscheme because tokyo_night is just superior.

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -4,8 +4,8 @@ vim.g.tokyonight_transparent = true
 vim.opt.background = "dark"
 
 function ApplyColorscheme(color)
-  color = color or "rose-pine"
+  color = color or "tokyonight-night"
   vim.cmd.colorscheme(color)
 end
 
-ApplyColorscheme("tokyonight-night")
+ApplyColorscheme()
