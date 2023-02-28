37872e56e7cdb024122fc0e94e23d92e5dd2f330
SzymonKubica <szymo.kubica@gmail.com>
2023-02-28 11:00:47 +0000

    Added snippets for elixir and latex and updated colorscheme.

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -5,7 +5,8 @@ vim.opt.background = "dark"
 
 function ApplyColorscheme(color)
   color = color or "rose-pine"
-  vim.opt.colorcolumn = "80"
+  -- Makes a wall at 81 so that it is obvious that characters can go up to 80.
+  vim.opt.colorcolumn = "81"
   vim.cmd.colorscheme(color)
 end
 
