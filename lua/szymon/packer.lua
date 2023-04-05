b6e1e08610df9910e9c97ad5fd19f2d7ea4ee729
SzymonKubica <szymo.kubica@gmail.com>
2023-04-05 22:10:11 +0200

    Added copilot extension

--- lua/szymon/packer.lua
+++ lua/szymon/packer.lua
@@ -6,6 +6,7 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 	use 'folke/tokyonight.nvim'
+  use 'github/copilot.vim'
   use({
     'rose-pine/neovim',
     as = 'rose-pine',
