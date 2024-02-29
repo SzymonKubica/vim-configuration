e9c6291cf9ee8914baea30344b341393cefd9ae5
SzymonKubica <szymo.kubica@gmail.com>
2024-02-29 11:20:17 +0000

    Constrained the auto close of backticks and single quotes because it was annoying.

--- after/plugin/autobrackets.lua
+++ after/plugin/autobrackets.lua
@@ -1,1 +1,7 @@
-require("autoclose").setup()
+require("autoclose").setup({
+   keys = {
+      ["'"] = { escape = false, close = false },
+      ["`"] = { escape = false, close = false },
+   },
+ })
+
