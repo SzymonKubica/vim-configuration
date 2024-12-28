eebf7386ba5da097d4df1fe6352302d5fd75e644
SzymonKubica <szymo.kubica@gmail.com>
2024-12-28 16:39:38 +0100

    Configure inlay hints to match the theme background

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -8,7 +8,9 @@ function ApplyColorscheme(color)
   -- Makes a wall at 81 so that it is obvious that characters can go up to 80.
   vim.opt.colorcolumn = "81"
   vim.cmd.colorscheme(color)
-  vim.api.nvim_set_hl(0, "LspInlayHint", { fg = "#9DA9A0" })
+  -- Get the background dynamically after setting the color scheme
+  local bg = vim.api.nvim_get_hl_by_name('Normal', true).background
+  vim.api.nvim_set_hl(0, "LspInlayHint", { bg = bg, fg = '#6e6a86' })
 end
 
 ApplyColorscheme("rose-pine")
